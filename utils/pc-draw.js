1d8825e0ae36b504918975b423747a7432a169cc
Lawrence-Clemente <lawrence.clemente.cohort225@gmail.com>
2024-06-10 02:20:26 -0700

    added notes

--- utils/pc-draw.js
+++ utils/pc-draw.js
@@ -10,7 +10,38 @@ idea
 3. write on that modal canvas
 4. append to canvas
 5. delete will remove that canvas => maybe need to refresh page?
+<script>
+let canvas = document.getElementById('myCanvas');
+let ctx = canvas.getContext('2d');
 
+let painting = false;
+
+function startDraw(e) {
+    painting = true;
+    draw(e);
+}
+
+function endDraw() {
+    painting = false;
+    ctx.beginPath();
+}
+
+function draw(e) {
+    if (!painting) return;
+    ctx.lineWidth = 10;
+    ctx.lineCap = 'round';
+    ctx.strokeStyle = 'black';
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+}
+
+canvas.addEventListener('mousedown', startDraw);
+canvas.addEventListener('mouseup', endDraw);
+canvas.addEventListener('mousemove', draw);
+</script>
 */
 
 function startDrawingPC(e, canvas, classname, x, y) {
